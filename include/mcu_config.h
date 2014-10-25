72087304a52e3b874e7ee2c98b6505f789ab3292
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-10-25 15:01:04 +0200

    Minor modifications for new hardware.

--- include/mcu_config.h
+++ include/mcu_config.h
@@ -54,15 +54,15 @@ extern "C" {
 
 // DEVCFG2
 #pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
-#pragma config FPLLMUL = MUL_24         // PLL Multiplier (24x Multiplier)
+#pragma config FPLLMUL = MUL_20         // PLL Multiplier (20x Multiplier)
 #pragma config UPLLIDIV = DIV_2         // USB PLL Input Divider (2x Divider)
 #pragma config UPLLEN = ON              // USB PLL Enable (Enabled)
 #pragma config FPLLODIV = DIV_1         // System PLL Output Clock Divider (PLL Divide by 1)
 
 // DEVCFG1
-#pragma config FNOSC = FRCPLL           // Oscillator Selection Bits (Fast RC Osc with PLL)
+#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
 #pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
-#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
 #pragma config POSCMOD = XT             // Primary Oscillator Configuration (XT osc mode)
 #pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
 #pragma config FPBDIV = DIV_8           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/8)
@@ -71,7 +71,7 @@ extern "C" {
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
 
 // DEVCFG0
-#pragma config DEBUG = ON               // Background Debugger Enable (Debugger is enabled)
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
 #pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
 #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
